6c20b0d3e96f9d62d539d0eedcabc1473c1dcab9
Sambuddha Basu <sambuddhabasu1@gmail.com>
2014-12-24 06:03:28 +0400

    Added sample home

--- home/urls.py
+++ home/urls.py
@@ -1,11 +1,10 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
+from home import views
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'opentainer.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^home/', include('home.urls')),
+    url(r'^$', views.home, name='home'),
 )
