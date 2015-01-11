868547667f8e16f78f3591fa24a4b9a46d7584f0
Sambuddha Basu <sambuddhabasu1@gmail.com>
2015-01-11 16:00:26 +0530

    Changed to home

--- Makefile
+++ Makefile
@@ -1,3 +1,11 @@
 .PHONY: docs
 docs:
 	cd docs && make html
+
+.PHONY: run
+run:
+	./manage.py runserver
+
+.PHONY: clean
+clean:
+	find . -name "*.pyc" -type f -delete
--- opentainer/urls.py
+++ opentainer/urls.py
@@ -6,6 +6,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'opentainer.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^home/', include('home.urls')),
+#    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', include('home.urls')),
 )
